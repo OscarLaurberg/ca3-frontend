55f50bfc2b23e61e47dda0e502f4f399d061c7fa
oscar laurberg <oscarlaurberg@hotmail.com>
2020-04-19 18:48:58 +0100

    first commit

---
+++ build/precache-manifest.90442f66dbc8ba841d7f238b6c5c8c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1b97ca702c155ac10f7f58aa580d5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dde26a55a68301947aec",
+    "url": "/static/css/main.702bd38c.chunk.css"
+  },
+  {
+    "revision": "5b0b100b877fc95d11b5",
+    "url": "/static/js/2.87ab0024.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.87ab0024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde26a55a68301947aec",
+    "url": "/static/js/main.7641cbd9.chunk.js"
+  },
+  {
+    "revision": "46c7eb3185e27f0478ef",
+    "url": "/static/js/runtime-main.95d61ddd.js"
+  }
+]);
